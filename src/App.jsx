a0a0ee9b0ff94bebc468170f01e2722effccee23
Mohammad-Alkreima <mohammadalkreima@gmail.com>
2025-08-03 22:00:04 +0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,16 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
-      <Footer />
+      <Footer image="FooterIcons/yourBankLogo.png"
+              links={[{url: "/" , content: "Home"}, {url: "/careers" , content: "Careers"}, {url: "/about" , content: "About"}, {url: "/security" , content: "Security"}]}
+              contacts={[{call: "mailto:",img: "FooterIcons/email.png", alt: "email logo", text: "hello@skillbirdge.com"},
+                          {call: "tel:",img: "FooterIcons/telephone.png", alt: "telephone logo", text: "+91 91813 23 2309"},
+                          {call: "https://2u.pw/c1nfT",img: "FooterIcons/location.png", alt: "location logo", text: "Somewhere in the World"}
+              ]}
+              socials={[{img: "FooterIcons/facebook.svg", alt: "facebook logo"}, {img: "FooterIcons/twitter.svg", alt: "twitter logo"}, {img: "FooterIcons/linkedin.svg", alt: "linkedin logo"}]}
+              copyRights="YourBank All Rights Reserved"
+              terms={[{content: "Privacy Policy"}, {content: "Terms of Service"}]}
+      />
     </>
   )
 }
