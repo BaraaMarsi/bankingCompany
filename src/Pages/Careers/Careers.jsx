30e7a8be1db89bdc799777d0df057eef6510fc50
NourAlsakkah <nouralsakkahnew@gmail.com>
2025-08-03 21:18:01 -0700

    startcomponent and button component

--- src/Pages/Careers/Careers.jsx
+++ src/Pages/Careers/Careers.jsx
@@ -1,8 +1,14 @@
+import Start from "../../Components/Start/Start";
 
 const Careers = () => {
     return (
         <div>
-
+            <Start 
+            title="Start your Career with " titlespan="Your Bank today!"
+            description="Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus."
+            buttonText="Open Accout"
+            buttonTo="/signup"
+            />
         </div>
     );
 }
