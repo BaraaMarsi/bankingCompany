b83e863c2b17096944f3076bcd5981970bc9d25c
NourAlsakkah <nouralsakkahnew@gmail.com>
2025-08-14 12:40:33 +0300

    adding section component to start compmnent and editing start css , finishing testimonials section

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
 import './Button.css';
 
-const Button = ({ text, to, className }) => {
+const Button = ({ text, to, className = '', onClick }) => {
   const combinedClass = `Na-linkbutton${className ? ' ' + className : ''}`;
 
-  return (
-    <div>
-      <Link to={to} className={combinedClass}>
-        {text}
-      </Link>
-    </div>
+
+  return to ? (
+    <Link to={to} className={combinedClass} onClick={onClick}>
+      {text}
+    </Link>
+  ) : (
+    <button className={combinedClass} onClick={onClick}>
+      {text}
+    </button>
   );
 };
 
