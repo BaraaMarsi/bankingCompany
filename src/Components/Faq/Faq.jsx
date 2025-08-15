a9ace3ecc5392e5e1f0da5446fc5984b6693df1b
AmmarAssad49 <assad192837@gmail.com>
2025-08-15 12:08:05 +0300

    commit

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -3,14 +3,14 @@ import Section from "../Section/Section";
 import TitleDev from "../TitleDev/TitleDev";
 import "./Faq.css";
 import { useNavigate } from "react-router-dom";
-import  FaqData  from "./../../Data/FaqData";
+import FaqData from "./../../Data/FaqData";
 
 
 const Faq = () => {
     const navigate = useNavigate();
     const [faqs, setfaqs] = useState(() => {
         const storedFaqs = localStorage.getItem('faqs');
-        return storedFaqs ? JSON.parse(storedFaqs) : [];
+        return storedFaqs ? JSON.parse(storedFaqs) : FaqData;
     })
     const [isOpen, setisOpen] = useState(false);
     const showFaqs = isOpen ? faqs : faqs.slice(0, 4);
@@ -22,8 +22,10 @@ const Faq = () => {
     }
     return (
         <>
-            <div className="BL-M container" onClick={handleContainerClick}>
-                <Section titleSpan={"Frequently"} secTitle={" Asked Questions"} desc={"Still you have any questions? Contact our Team via support@yourbank.com"} />
+            <div className="Am-M container" onClick={handleContainerClick}>
+                <div className="Am-faqTitle">
+                    <Section titleSpan={"Frequently"} secTitle={" Asked Questions"} desc={"Still you have any questions? Contact our Team via support@yourbank.com"} />
+                </div>
                 {
                     showFaqs.length === 0 ?
                         <p> There Is No Data</p> :
@@ -31,6 +33,7 @@ const Faq = () => {
                             <TitleDev key={index} title={faq.question} div1={faq.answer} />
                         ))
                 }
+
             </div>
             <button onClick={() => setisOpen(!isOpen)}>{buttonText}</button>
         </>
