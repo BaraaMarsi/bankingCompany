8c8ff901e07a629e8e6919325a40650727af5c77
NourAlsakkah <nouralsakkahnew@gmail.com>
2025-08-18 14:09:59 +0300

    job openings

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -217,7 +217,9 @@ const Home = () => {
                 </div>
             </section>
             <Faq />
-            <Testimonials />
+            <div className="container">
+                <Testimonials />
+            </div>
             <Start
                 title="Start your financial journey with " titlespan="Your Bank today!"
                 description="Ready to take control of your finances? Join YourBank now, and let us help you achieve your financial goals with our tailored solutions and exceptional customer service"
