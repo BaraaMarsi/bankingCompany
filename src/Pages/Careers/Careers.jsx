f16b0b03755b51fcf3712bae135b01958905debe
NourAlsakkah <nouralsakkahnew@gmail.com>
2025-08-22 14:58:56 +0300

    job

--- src/Pages/Careers/Careers.jsx
+++ src/Pages/Careers/Careers.jsx
@@ -1,9 +1,6 @@
 import Hero from "../../Components/Hero/Hero.jsx";
 import "./Careers.css"
-<<<<<<< HEAD
-=======
 import OurBenefits from "../../Components/OurBenefits/OurBenefits.jsx"
->>>>>>> 1f3f1b4d5f5b67a9276182e8dc071c475e8fba52
 import StartSection from "../../Components/StartSection/StartSection";
 import Faq from "../../Components/Faq/Faq.jsx";
 import OurValuesSection from "../../Components/OurValuesSection/OurValuesSection.jsx";
